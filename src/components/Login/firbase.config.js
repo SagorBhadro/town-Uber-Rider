f3e6752bcb83d39dda3c31c20dcf041601dd969c
Sagor Bhadro <sagorbhadro553311@gmail.com>
2021-03-21 02:22:26 +0600

    new

---
+++ src/components/Login/firbase.config.js
@@ -0,0 +1,9 @@
+const firebaseConfig = {
+  apiKey: "AIzaSyDpL2heOHreiaHzjkNmqk_9HHTHg_t7iPs",
+  authDomain: "pick-me01.firebaseapp.com",
+  projectId: "pick-me01",
+  storageBucket: "pick-me01.appspot.com",
+  messagingSenderId: "214430650270",
+  appId: "1:214430650270:web:55ce1f1799bad9724d108f"
+};
+export default firebaseConfig;
